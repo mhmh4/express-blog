9c3fe9a9d035dd015f2cdac0a6236e450684eee6
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-08-14 18:09:47 -0400

    Update app.js

--- api/app.js
+++ api/app.js
@@ -1,1 +1,29 @@
-console.log("test");
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import jwt from "jsonwebtoken";
+
+const app = express();
+const port = 3000;
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+
+function generateAccessToken(username) {
+  return jwt.sign(username, process.env.SECRET_KEY, { expiresIn: "1800s" });
+}
+
+app.post("/api/users/new", (req, res) => {
+  const token = generateAccessToken({ username: req.body.username });
+  res.json(token);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});

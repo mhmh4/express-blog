e21a974d87351ff2b0ed99259087ab9d64a32a9f
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-09-15 20:03:08 -0400

    Remove body-parser import

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import path from "path";
 import { fileURLToPath } from "url";
 
-import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";

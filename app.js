470f05a3b2ba3ed39e4fc0dbd0869bb4a43a220f
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-09-18 13:01:45 -0400

    Remove unused LOC

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import nunjucks from "nunjucks";
-import jwt from "jsonwebtoken";
 import nocache from "nocache";
 import { PrismaClient } from "@prisma/client";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
@@ -19,30 +12,12 @@ const prisma = new PrismaClient();
 
 dotenv.config();
 
-// temporary data
-
-const posts = [
-  { title: "title 1", content: "content 1" },
-  { title: "title 2", content: "content 2" },
-  { title: "title 3", content: "content 3" },
-];
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// app.use(express.static("./views"));
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(nocache());
 
 nunjucks.configure("views", { express: app, watch: true });
-
-// function generateAccessToken(username) {
-//   return jwt.sign(username, process.env.SECRET_KEY, { expiresIn: "1800s" });
-// }
-
-// app.post("/api/users/new", (req, res) => {
-//   const token = generateAccessToken({ username: req.body.username });
-//   res.json({ token: token });
-// });
 
 app.get("/", (req, res) => {
   return res.redirect("/posts");

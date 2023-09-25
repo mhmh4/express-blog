492e8abda56d4cd63276944aecca340a061f5c09
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-09-24 20:01:13 -0400

    Simplify app.js

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 import cors from "cors";
-import dotenv from "dotenv";
+import dotenv from "dotenv/config";
 import express from "express";
 import nunjucks from "nunjucks";
 import nocache from "nocache";
-import { PrismaClient } from "@prisma/client";
 
 import authRoutes from "./routes/auth.js";
 import postRoutes from "./routes/posts.js";
@@ -11,20 +10,16 @@ import postRoutes from "./routes/posts.js";
 const app = express();
 const port = 3000;
 
-const prisma = new PrismaClient();
-
-dotenv.config();
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(nocache());
 
-nunjucks.configure("views", { express: app, watch: true });
+nunjucks.configure("./views", { express: app, watch: true });
 
 app.use("/", authRoutes);
 app.use("/posts", postRoutes);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log("Listening on port " + port);
 });

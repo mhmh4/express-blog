e5f74aba7401e8af8d1391af204fdd6d9ab44b90
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-09 07:43:55 -0400

    Reorder import and remove JSON middleware

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv/config";
 import express from "express";
@@ -6,14 +7,12 @@ import nocache from "nocache";
 
 import authRoutes from "./routes/auth.js";
 import postRoutes from "./routes/posts.js";
-import cookieParser from "cookie-parser";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cookieParser());
 app.use(cors());
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(nocache());
 
